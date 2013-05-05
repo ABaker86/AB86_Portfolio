b249752aac91a50a40fc896202ad895f01b09f3c
Adam Baker <adambaker@live.com>
2013-05-05 14:00:14 -0400

    Bool and Char in CPP
    Two different programs that display how to use bool and char type in
    cpp.

---
+++ CPP/1002bool.cpp
@@ -0,0 +1,21 @@
+//////////
+//Bool Utilization
+//5/5/2013
+//////////
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    bool Lillith;
+    Lillith = 0;
+
+    if(Lillith)
+        cout << "Lillith rules apply!\n";
+        else
+        cout << "Xavier rules apply!\n";
+
+
+    return 0;
+}
---
+++ CPP/1003CharDT.cpp
@@ -0,0 +1,28 @@
+//////////
+//Char Data Type
+//5/5/2013
+//////////
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    char c;
+    // prints one letter at a time
+    c = 'X';
+    cout << c;
+    c = 'a';
+    cout << c;
+    c = 'v';
+    cout << c;
+    c = 'i';
+    cout << c;
+    c = 'e';
+    cout << c;
+    c = 'r';
+    cout << c;
+    cout << " \n";
+
+
+    return 0;
+}

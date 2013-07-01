c55cd379647f843ecf53deababb753938884269b
Adam Baker <adambaker@live.com>
2013-07-01 18:34:37 -0400

    First commit of NoCream from Eclipse with Aptana Git plugin

---
+++ NoCream/bin/FirstPush.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Adam
+ *
+ */
+public class FirstPush {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is the first push with java from Eclipse");
+
+	}
+
+}
